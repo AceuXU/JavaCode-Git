1deabef63fa88a069b7c03bb8bbab6ccc55607a3
Aceu <ratanrathod870@gmail.com>
2023-07-13 19:36:12 +0530

    created a overloaded constructors

---
+++ OverLoaded Constructors/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+      Pizza pizza = new Pizza("thicc crust", "tomato","mozzerella","pepperoni");
+
+      System.out.println("Here are the ingredients of your pizza");
+
+        System.out.println(pizza.bread);
+        System.out.println(pizza.cheese);
+        System.out.println(pizza.topping);
+        System.out.println(pizza.sauce);
+
+
+    }
+}
