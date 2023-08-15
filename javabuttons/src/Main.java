6a9afaf92533297e46567c07054f10495a3c09d1
Aceu <ratanrathod870@gmail.com>
2023-08-15 16:21:20 +0530

    button and checkbox

---
+++ javabuttons/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // a button that performs an action when clicked on
+
+        new MyFrame();
+
+    }
+}
---
+++ javabuttons/src/MyFrame.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MyFrame extends JFrame {
+
+    JButton button;
+    MyFrame(){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(null);
+        this.setSize(500,500);
+
+        button = new JButton();
+        button.setBounds(100,100,200,150);
+        button.setText("Button");
+        button.addActionListener(e -> System.out.println("boom"));
+        button.setFocusable(false);
+        button.setFont(new Font("Comic Sans",Font.BOLD,25));
+        button.setForeground(Color.cyan);
+        button.setBackground(Color.BLACK);
+        button.setBorder(BorderFactory.createEtchedBorder());
+        button.setEnabled(false);  // to disable button
+
+        this.add(button);
+        this.setVisible(true);
+
+    }
+}
