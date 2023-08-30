2fa47a5cdb8e0690904d05b968c231512f89e7b1
Aceu <ratanrathod870@gmail.com>
2023-08-30 15:42:06 +0530

    BubbleSort

---
+++ BubbleSortDS/src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    // bubble sort = pairs of adjacent elements are compared, and the elements swapped if they are not in order.
+    //	Quadratic time O(n^2)
+    //	small data set = okay-ish
+    //	large data set = BAD
+    public static void main(String[] args) {
+        int array[] = {8,1,3,4,2,5,6,9,7,8};
+
+        bubbleSort(array);
+        for (int i : array){
+            System.out.print(i);
+        }
+    }
+    public static void bubbleSort(int array[]){
+        for (int i = 0; i < array.length -1; i++){
+            for (int j = 0; j< array.length - i - 1; j++){
+                if (array[j] > array[j+1]){
+                    int temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
+
+                }
+            }
+        }
+    }
+}
