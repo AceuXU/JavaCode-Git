d4867e21992e28b406b0a10a572027fa7594968c
Aceu <ratanrathod870@gmail.com>
2023-09-02 15:32:49 +0530

    AdjacnecyListDS

---
+++ AdjacencyLists/src/Main.java
@@ -0,0 +1,34 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Adjacency List = An array/arraylist of Linked-lists.
+
+        // Each LinkedList has a unique node at the head.
+
+        // All adjacent neighbors to that node are added to that node's Linked-list
+
+        // runtime complexity to check an Edge: O(v)
+        // space complexity: O(v + e)
+
+        Graph graph = new Graph();
+
+        graph.addNode(new Node('A'));
+        graph.addNode(new Node('B'));
+        graph.addNode(new Node('C'));
+        graph.addNode(new Node('D'));
+        graph.addNode(new Node('E'));
+
+        graph.addEdge(0,1);
+        graph.addEdge(1,2);
+        graph.addEdge(1,4);
+        graph.addEdge(2,4);
+        graph.addEdge(2,3);
+        graph.addEdge(4,1);
+        graph.addEdge(4,2);
+        graph.addEdge(3,1);
+
+        graph.print();
+
+    }
+}
