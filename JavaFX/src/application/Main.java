7805fc1dee17148aef0879ffd8bebd4126760364
Aceu <ratanrathod870@gmail.com>
2023-09-08 15:14:41 +0530

    Quiz Game

---
+++ JavaFX/src/application/Main.java
@@ -0,0 +1,16 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main {
+    public static void main(String[] args) {
+        launch(args);
+
+    }
+
+    public void start(Stage stage) throws Exception{
+//        Stage stage = new Stage();
+
+        stage.show();
+
+    }
+}
---
+++ JavaFX/src/application/application/css.java
@@ -0,0 +1,2 @@
+package application.application;public class css {
+}
